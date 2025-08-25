c535276d49e3168b3c3fbd96789fb642ca19b548
manisaireddysomala <manisaireddysomala@gmail.com>
2025-08-25 11:41:28 +0530

    Initial Files

---
+++ src/main/java/com/example/money_mhttps/start/spring/io/anager/service/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.example.money_mhttps.start.spring.io.anager.service;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
